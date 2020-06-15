db28195eabb08f32cac2ee2e599142d0725685f5
Matt Ferrin <mattferrin@gmail.com>
2020-06-14 20:48:32 -0700

    comment store hook code

--- src/store/buildUseStore.ts
+++ src/store/buildUseStore.ts
@@ -1,14 +1,20 @@
 import { useEffect, useState, Dispatch } from "react";
 
+// helps others insure continuous uninterrupted paths through their code
+// ... helps result in a final calculated value (exhaustiveness checking)
 class ExhaustiveReturns {}
 
+// setter type
 type SetStoreInput<State> = (state: State) => State;
 export type SetStore<State> = (
   newValue: SetStoreInput<State>
 ) => ExhaustiveReturns;
 
+// hook type
 type UseStoreInput<State, SubState> = (state: State) => SubState;
 type UseStoreOutput<State, SubState> = [SubState, SetStore<State>];
+
+// returns hook type
 type BuildUseStoreOutput<State> = <SubState>(
   initialValue: UseStoreInput<State, SubState>
 ) => UseStoreOutput<State, SubState>;
@@ -25,32 +31,41 @@ export function buildUseStore<State>(
   let forceUpdates: Dispatch<State>[] = [];
   let selectors: Dispatch<State>[] = [];
 
+  /**
+   * @param A selector function (similar to Redux useSelector).
+   * @return A computed value from state, and a state setter.
+   */
   return (selector) => {
     const [, forceUpdate] = useState();
     useEffect(() => {
       if (!forceUpdates.includes(forceUpdate)) {
+        // add setState to list, so calling it triggers rerender
         forceUpdates.push(forceUpdate);
       }
       if (!selectors.includes(selector)) {
+        // add selector to list, so rerender can be skipped if the computed value is unchanged
         selectors.push(selector);
       }
       return () => {
+        // remove unmounted component from lists
         forceUpdates = forceUpdates.filter((x) => x !== forceUpdate);
         selectors = selectors.filter((x) => x !== selector);
       };
     }, [forceUpdate, selector]);
 
     return [
+      // computed value from state
       selector(value),
+      // setter for state
       (newValue) => {
         const nextValue = newValue(value);
-        forceUpdates.forEach((x, i) => {
+        forceUpdates.forEach((setValue, i) => {
           if (selectors[i](nextValue) !== selectors[i](value)) {
-            console.log({ i });
-            x(newValue(value));
+            // useState setter triggers rerender with new value
+            setValue(nextValue);
           }
         });
-        value = newValue(value);
+        value = nextValue;
         return new ExhaustiveReturns();
       },
     ];
